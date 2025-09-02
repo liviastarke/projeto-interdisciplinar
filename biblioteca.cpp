bc9d425b670bcf2ddb8750ae1794092a1e6233b3
livia <liviastarke@gmail.com>
2025-09-02 12:58:43 -0300

    using constructors and encapsulation

--- biblioteca.cpp
+++ biblioteca.cpp
@@ -18,32 +18,73 @@ Físico e Livro digital, com atributos e métodos específicos.*                
 #include <iostream>
 
 using namespace std;
-
-class Livro {
-    public:
-        string titulo; // atributos título, autor e gênero
-        string autor;
-        string genero;
-        void method(/* args */){ // método definido dentro da classe
-            cout << "Flap flap, páginas virando" << endl;
-        }
-        void outClassDef();  // definição fora da classe
+class biblioteca
+{
+private:
+    /* data */
+public:
+    biblioteca(/* args */);
+    ~biblioteca();
 };
 
-void Livro::outClassDef(/* args */) {
+biblioteca::biblioteca(/* args */)
+{
+}
+
+biblioteca::~biblioteca()
+{
+}
+
+class Livro {
+    private:
+    // atributos título, autor e gênero são privados
+        string titulo;
+        string autor;
+        string genero;
+    public:
+    // os métodos são públicos
+        // Declaração do construtor (definido fora da classe)
+        Livro(string tit, string aut, string gen);
+
+        // getters
+        string get_titulo(){
+            return titulo;
+        }
+
+        string get_autor(){
+            return autor;
+        }
+
+        string get_genero(){
+            return genero;
+        }
+
+        // outros métodos
+        void ler(/* args */){ // método definido dentro da classe
+            cout << "Flap flap, páginas virando" << endl;
+        }
+
+        void soltar();  // definição fora da classe
+};
+
+Livro::Livro(string tit, string aut, string gen){
+    titulo = tit;
+    autor = aut;
+    genero = gen;
+}
+
+void Livro::soltar(/* args */) {
     cout << "Bum! Caiu no chão" << endl;
 }
 
 int main(){
-    Livro hobbit; // objeto hobbit é da classe livro
-
-    hobbit.titulo = "O Hobbit"; // definindo atributos
-    hobbit.autor = "J. R. R. Tolkien";
-    hobbit.genero = "Fantasia";
+    // objeto hobbit da classe livro, definindo atributos com o construtor da classe
+    Livro hobbit("O Hobbit", "J. R. R. Tolkien", "Fantasia");
     
-    hobbit.method(); // chamando um método
-    hobbit.outClassDef(); // outro método
+    hobbit.ler(); // chamando um método
+    hobbit.soltar(); // outro método
 
-    cout << "Livro 1\t\t" << hobbit.titulo << ": " << hobbit.autor << endl;
+    // get título e autor
+    cout << "Livro 1\t\t" << hobbit.get_titulo() << ": " << hobbit.get_autor() << endl;
     return 0;
 }
